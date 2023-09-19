092dceb58c851481a0c2750aec002e493b749a5d
CrippledJevil <81448991+CrippledJevil@users.noreply.github.com>
2023-09-19 11:21:33 -0400

    e

--- Crayon.java
+++ Crayon.java
@@ -4,5 +4,14 @@ import java.math.*;
 
 public class CrayonTime {
     int x = math.random;
-    a
+    int strx = math.random()*100;
+    String[] strs = new String[strx];
+    String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for(int i = 0; i < strs.length; i++)
+    {
+        for(int j = 0; j < math.random()*10; j++)
+        {
+            strs[i] += abc.charAt((int) math.random()*26);
+        }
+    }
 }
